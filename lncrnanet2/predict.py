0287138f6e9802225f70d9a13f00f121ec1930a9
Yashpal Ramakrishnaiah <yashpal.ramakrishnaiah1@monash.edu>
2023-06-27 17:00:29 +1000

    removed a separate file

--- lncrnanet2/predict.py
+++ lncrnanet2/predict.py
@@ -64,11 +64,10 @@ def predict_lncrnanet2(seq):
     elif len(seq) > 3000:
         return "Sequence length more than 3000"
     else:
-        import stopmodel as stopmodel
         orf=[]
         seq = seq.replace('U', 'T')
         seq = char_to_int(seq)
-        results = stopmodel.findORF(seq)
+        results = findORF(seq)
         start=results[0]
         stop=results[1]
         cur_orf=np.ones(stop-start)
@@ -120,6 +119,50 @@ class CharacterTable(object): #make encoding table
         if calc_argmax:
             X = X.argmax(axis=-1)
         return ''.join(self.indices_char[x] for x in X)
+
+
+#!/usr/bin/python3
+
+import os
+import sys
+import numpy as np
+
+#keras
+os.environ['KERAS_BACKEND'] = 'theano'
+from keras.models import load_model
+from keras.utils import pad_sequences
+
+sys.setrecursionlimit(100000)
+
+def findORF(seq):
+    orflen=0
+    orf=""
+    o_s=0
+    o_e=0
+    length=len(seq)
+    seq=[seq]
+    seq= pad_sequences(seq,maxlen=length,padding='post')
+    seq=(np.arange(seq.max()+1) == seq[:,:,None]).astype(dtype='float32')
+    seq=np.delete(seq,0,axis=-1)
+    for frame in range(3):
+        tseq=stopmodel.predict(seq[:,frame:])[:,:(length-frame)//3]
+        tseq=np.argmax(tseq,axis=-1)-1
+        sseq=np.append(-1,np.where(tseq==1)[1])
+        sseq=np.append(sseq,tseq.shape[1])
+        lseq=np.diff(sseq)-1
+        flenp=np.argmax(lseq)
+        flen=lseq[flenp]
+        n_s=frame+3*sseq[flenp]+3
+        n_e=frame+3*sseq[flenp+1]
+        
+        if flen>orflen or ((orflen==flen) and n_s<o_s):
+            orflen=flen
+            o_s=n_s
+            o_e=n_e
+
+    return o_s,o_e
+
+stopmodel=load_model('data/stopfinder_singleframe.h5')
 
 
 if __name__ == '__main__':
